494d64e48f5c86267804ab822c024de7619c4bd9
Lakshya Garg <lakshya.garg2004@gmail.com>
2024-04-08 22:35:34 +0530

    Added Hospitals data

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const port=4000;
 
 const disease=require("./MOCK_DATA (6).json")
 
+const hospitals=require("./hospitals.json")
+
 
 
 app.use(express.static(path.join(__dirname,"public")))
@@ -17,18 +19,36 @@ app.use(express.static(path.join(__dirname,"public")))
 app.use(express.urlencoded({extended:false}))
 
 
-app.get("/disease",(req,res)=>{
-    const html=`<ul>
-    ${disease.map((dis)=>{
-        `<li>${dis.disease_name}</li>`
-    
-    })}</ul>`
-    res.send(html)
-})
+// app.get("/disease",(req,res)=>{
+//     const html=`
+//     <ul>
+//     ${disease.map((dis)=>{
+//         `<li>${dis.name}</li>`})}
+//     </ul>`
+//     res.send(html)
+// })
 
 
 app.get("/api/disease",(req,res)=>{
     return res.json(disease)
+
+})
+app.get("/api/hospitals",(req,res)=>{
+    return res.json(hospitals)
+
+})
+app.get("/api/disease/:id",(req,res)=>{
+    
+    const id=Number(req.params.id);
+    const problem=disease.find((user)=>user.id===id);
+    return res.json(problem);
+
+})
+app.get("/api/hospitals/:id",(req,res)=>{
+    
+    const id=Number(req.params.id);
+    const hosp=hospitals.find((use)=>use.id===id);
+    return res.json(hosp);
 
 })
 
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+app.get("/disease",(req,res)=>{
+//     const html=`
+//     <ul>
+//     ${disease.map((dis)=>{
+//         `<li>${dis.name}</li>`})}
+//     </ul>`
+//     res.send(html)
+// })
